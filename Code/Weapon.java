91298fa921e84f6c61c727a5b451e6bd40d4d926
t00182938 <dclocky1@gmail.com>
2016-11-17 14:31:03 +0000

    Created the class "Weapon" to be used to create weapons for the player to equip and store in there inventory.Constructors getters and setters where set in the class

--- Code/Weapon.java
+++ Code/Weapon.java
@@ -1,12 +1,53 @@
 
 
 public class Weapon{
+
+    double damage;
+    boolean isSharp,isHeavy,isEnchanted;
+
     public Weapon()
     {
 
     }
-    public Weapon()
+    public Weapon(boolean isSharp, boolean isHeavy, boolean isEnchanted, double damage)
     {
-
+        setSharp(isSharp);
+        setHeavy(isHeavy);
+        setEnchanted(isEnchanted);
+        setDamage(damage);
     }
+
+    public double getDamage() {
+        return damage;
+    }
+
+    public void setDamage(double damage) {
+        this.damage = damage;
+    }
+
+    public boolean isSharp() {
+        return isSharp;
+    }
+
+    public void setSharp(boolean sharp) {
+        isSharp = sharp;
+    }
+
+    public boolean isHeavy() {
+        return isHeavy;
+    }
+
+    public void setHeavy(boolean heavy) {
+        isHeavy = heavy;
+    }
+
+    public boolean isEnchanted() {
+        return isEnchanted;
+    }
+
+    public void setEnchanted(boolean enchanted) {
+        isEnchanted = enchanted;
+    }
+
+
 }
