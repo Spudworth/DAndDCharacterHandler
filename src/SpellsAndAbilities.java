406462c373e5a183122220ae3ae741ca27986ab9
t00141773 <Sean.Healy@students.ittralee.ie>
2016-11-28 15:37:07 +0000

    Fifth commit: Added new class  SpellsAndAbilities and test class. Class contains getters, setters, and attributes for both spells and abilities.

---
+++ src/SpellsAndAbilities.java
@@ -0,0 +1,62 @@
+
+
+public class SpellsAndAbilities {
+
+
+    private String spellName;
+    private String spellDescription;
+    private String damageType;
+    private int damage;
+
+    private String abilityName;
+    private String abilityDescription;
+
+    public String getSpellName() {
+        return spellName;
+    }
+
+    public void setSpellName(String spellName) {
+        this.spellName = spellName;
+    }
+
+    public String getSpellDescription() {
+        return spellDescription;
+    }
+
+    public void setSpellDescription(String spellDescription) {
+        this.spellDescription = spellDescription;
+    }
+
+    public String getDamageType() {
+        return damageType;
+    }
+
+    public void setDamageType(String damageType) {
+        this.damageType = damageType;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public String getAbilityName() {
+        return abilityName;
+    }
+
+    public void setAbilityName(String abilityName) {
+        this.abilityName = abilityName;
+    }
+
+    public String getAbilityDescription() {
+        return abilityDescription;
+    }
+
+    public void setAbilityDescription(String abilityDescription) {
+        this.abilityDescription = abilityDescription;
+    }
+
+}
---
+++ test/SpellsAndAbilitiesTest.java
@@ -0,0 +1,65 @@
+import static org.junit.Assert.*;
+
+
+public class SpellsAndAbilitiesTest {
+    @org.junit.Test
+    public void getSpellName() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void setSpellName() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getSpellDescription() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void setSpellDescription() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getDamageType() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void setDamageType() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getDamage() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void setDamage() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getAbilityName() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void setAbilityName() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getAbilityDescription() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void setAbilityDescription() throws Exception {
+
+    }
+
+}
