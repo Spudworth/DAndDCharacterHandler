d3cc58d229015329e697dfbdcb53a44ca4dbfad1
Sean Healy <Sean.Healy@students.ittralee.ie>
2016-12-09 18:17:22 +0000

    Eighth Commit: Completed the test cases for the Abilities class. Four test methods testing the two methods of the class.

--- test/AbilitiesTest.java
+++ test/AbilitiesTest.java
@@ -1,3 +1,5 @@
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -6,13 +8,42 @@ import static org.junit.Assert.*;
  * Created by X99-A on 09-Dec-16.
  */
 public class AbilitiesTest {
+
     @Test
-    public void getIsPassive() throws Exception {
+    public void getIsPassivePass() throws Exception {
+
+        Abilities ability = new Abilities("Stealth", "You are Sneaky",true);
+        Boolean isPassiveTest = ability.getIsPassive();
+        Assert.assertEquals(isPassiveTest,true);
 
     }
 
     @Test
-    public void setIsPassive() throws Exception {
+    public void getIsPassiveFail() throws Exception {
+
+        Abilities ability = new Abilities("Stealth", "You are Sneaky",true);
+        Boolean isPassiveTest = ability.getIsPassive();
+        Assert.assertNotEquals(isPassiveTest,false);
+
+    }
+
+    @Test
+    public void setIsPassivePass() throws Exception {
+
+        Abilities ability = new Abilities("Stealth", "You are Sneaky",true);
+        ability.setIsPassive(false);
+        Boolean isPassiveTest = ability.getIsPassive();
+        Assert.assertEquals(isPassiveTest,false);
+
+    }
+
+    @Test
+    public void setIsPassiveFail() throws Exception {
+
+        Abilities ability = new Abilities("Stealth", "You are Sneaky",true);
+        ability.setIsPassive(false);
+        Boolean isPassiveTest = ability.getIsPassive();
+        Assert.assertNotEquals(isPassiveTest,true);
 
     }
 
