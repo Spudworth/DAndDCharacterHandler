9574672ff2521a29cb3423ab5d332e56f4ec9d81
t00141773 <Sean.Healy@students.ittralee.ie>
2016-12-12 09:37:04 +0000

    Fifteenth Commit: Moved method initialiseInventory from the MenuGUI class to the Inventory class.

--- src/Inventory.java
+++ src/Inventory.java
@@ -17,6 +17,8 @@ public class Inventory {
         weaponsEquipped  = new ArrayList<Weapon>();
     }
 
+
+
     public void addSpells(String spellName, String spellDescription, String damageType, int damage){
         Spells spell = new Spells(spellName,spellDescription,damageType,damage);
         spells.add(spell);
@@ -89,5 +91,28 @@ public class Inventory {
         }
         else
             System.out.println("You already have a weapon Equipped");
+    }
+    protected void initialiseInventory(){
+
+
+        addAbilities("Stealth","Ability to sneak", true);
+        addAbilities("Climb","Ability to scale obstacles",false);
+        addAbilities("Swim","Ability to cross rivers",false);
+        addAbilities("Night Vision","Ability to see in the dark",true);
+
+        addSpells("Fireball","Casts a fireball at target","Fire",8);
+        addSpells("Lightning bolt","Casts lightning at target","Electric",8);
+        addSpells("Ice spike","Casts a spike of ice at target","Ice",8);
+        addSpells("Blindness","Blinds the target","Dark",0);
+
+        addTools("Shovel","Able to dig holes",4);
+        addTools("Rope","Able to bind items or climb obstacles",2);
+        addTools("Pickaxe","Able to split stone",6);
+        addTools("Lantern","Able to highlight dark areas",4);
+
+        addWeapon("Longsword",true,true,false,20);
+        addWeapon("Mace",false,true,false,20);
+        addWeapon("Staff",false,false,true,15);
+        addWeapon("Bow",false,false,false,12);
     }
 }
--- src/MenuGUI.java
+++ src/MenuGUI.java
@@ -27,7 +27,7 @@ public class MenuGUI extends JFrame implements ActionListener{
         menuBar.add(weaponMenu);
         menuBar.add(spellsMenu);
         menuBar.add(abilitiesMenu);
-        initialiseInventory(inventory);
+        inventory.initialiseInventory();
     }
     private void createWeaponMenu() {
 
@@ -109,30 +109,6 @@ public class MenuGUI extends JFrame implements ActionListener{
 
     private void showMessage(String s) {
         JOptionPane.showMessageDialog(null,s);
-    }
-
-    private static void initialiseInventory(Inventory inventory){
-
-
-        inventory.addAbilities("Stealth","Ability to sneak", true);
-        inventory.addAbilities("Climb","Ability to scale obstacles",false);
-        inventory.addAbilities("Swim","Ability to cross rivers",false);
-        inventory.addAbilities("Night Vision","Ability to see in the dark",true);
-
-        inventory.addSpells("Fireball","Casts a fireball at target","Fire",8);
-        inventory.addSpells("Lightning bolt","Casts lightning at target","Electric",8);
-        inventory.addSpells("Ice spike","Casts a spike of ice at target","Ice",8);
-        inventory.addSpells("Blindness","Blinds the target","Dark",0);
-
-        inventory.addTools("Shovel","Able to dig holes",4);
-        inventory.addTools("Rope","Able to bind items or climb obstacles",2);
-        inventory.addTools("Pickaxe","Able to split stone",6);
-        inventory.addTools("Lantern","Able to highlight dark areas",4);
-
-        inventory.addWeapon("Longsword",true,true,false,20);
-        inventory.addWeapon("Mace",false,true,false,20);
-        inventory.addWeapon("Staff",false,false,true,15);
-        inventory.addWeapon("Bow",false,false,false,12);
     }
 
 }
